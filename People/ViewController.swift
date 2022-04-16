1ddf4aeda292ab1204006b168a7946f6b5a04689
Fawzi <fawzialrifai@icloud.com>
2022-04-16 20:27:08 +0300

    Completed.

--- People/ViewController.swift
+++ People/ViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Database.database().reference().child("james").child("children").observe(.value) { snapshot in
+            print("number of james children = \(snapshot.childrenCount)") // observe and print james children count
+        }
+        Database.database().reference().child("james").child("children").queryOrdered(byChild: "name").queryEqual(toValue: "child2").getData { error, snapshot in
+            if snapshot.exists() {  // search for james child named child2
+                print("found")
+            } else {
+                print("not found")
+            }
+        }
     }
 
 

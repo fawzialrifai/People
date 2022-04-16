ce8b4a00d117a34d1c2982c8c19ec3958ba84b95
Fawzi <fawzialrifai@icloud.com>
2022-04-16 19:49:49 +0300

    Initial Commit

---
+++ People/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  People
+//
+//  Created by Fawzi Rifai on 16/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
